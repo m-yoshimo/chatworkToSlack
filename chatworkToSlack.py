4f1d544763156df177992c83e6fa2c30c3e9f193
Masanori Yoshimoto <masanori.yoshimoto@zenkigen.jp>
2019-05-09 08:19:02 +0900

    Mention to slack from chatwork TO

--- chatworkToSlack.py
+++ chatworkToSlack.py
@@ -12,48 +12,55 @@ cwapiurl = 'https://api.chatwork.com/v2'
 slapiurl = 'https://slack.com/api'
 
 def lambda_handler(event, context):
-  fromto = event['fromto']
+    fromto = event['fromto']
+    mentions = event['mention']
 
-  for list in fromto:
-    roomid  = list['cwroomid']
-    channel = list['slchannel']
+    for list in fromto:
+        roomid  = list['cwroomid']
+        channel = list['slchannel']
 
-    url1     = cwapiurl + '/rooms/' + roomid + '/messages'
-    headers1 = {'X-ChatWorkToken': cwtoken }
+        url1     = cwapiurl + '/rooms/' + roomid + '/messages'
+        headers1 = {'X-ChatWorkToken': cwtoken }
 
-    try :
-       req1 = requests.get(url1, headers=headers1)
-       if req1.status_code == 204 :
-          print('204' )
-       else :
-          res_json=req1.json()
-          i = 0
-          l = len(res_json) - 1
-          while i <= l :
-             body = res_json[i]['body']
-             name = res_json[i]['account']['name']
-             msid = res_json[i]['message_id']
-             time = res_json[i]['send_time']
+        try :
+            req1 = requests.get(url1, headers=headers1)
+            if req1.status_code == 204 :
+                print('204' )
+            else :
+                res_json = req1.json()
+                i = 0
+                l = len(res_json) - 1
+                while i <= l :
+                    body = res_json[i]['body']
+                    name = res_json[i]['account']['name']
+                    msid = res_json[i]['message_id']
+                    time = res_json[i]['send_time']
 
-             try :
-                text = '*' + str(datetime.fromtimestamp(time)) + ' message from: ' + name + '*\n```' + body + '```'
-                url2    = slapiurl + '/chat.postMessage'
-                params2 = {
-                   'token' : sltoken,
-                   'channel' : channel,
-                   'text' : text,
-                   'as_user': 'false',
-                   'username': 'ChatWork(auto)'
-                }
-                req2 = requests.put(url2, params=params2)
+                    try :
+                        text = '*' + str(datetime.fromtimestamp(time)) + ' message from: ' + name + '*\n'
+                        for mention in mentions:
+                            cw_to = mention['cwto']
+                            sl_to = mention['slto']
+                            if body.find(cw_to) > 0 :
+                                text += '<@' + sl_to + '>\n'
+                        text += '```' + body + '```'
+                        url2 = slapiurl + '/chat.postMessage'
+                        params2 = {
+                            'token' : sltoken,
+                            'channel' : channel,
+                            'text' : text,
+                            'as_user': 'false',
+                            'username': 'ChatWork(auto)'
+                        }
+                        req2 = requests.put(url2, params=params2)
 
-             except Exception as e :
-                print('[error][put]' + str(e))
+                    except Exception as e :
+                        print('[error][put]' + str(e))
 
-             i += 1
+                    i += 1
 
-    except Exception as e :
-       print('[error][get]' + str(e))
+        except Exception as e :
+            print('[error][get]' + str(e))
 
 if __name__ == "__main__":
     lambda_handler({}, {})
